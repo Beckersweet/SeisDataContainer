b9fa501a31a9284797ddc7f65da1d441486bda06
Henryk Modzelewski <henryk_modzelewski@mac.com>
2011-06-11 16:58:26 -0700

    changed DataContainer.io.emptyHeaderStruct to DataContainer.io.minimalHeaderStruct

---
+++ +DataContainer/+io/minimalHeaderStruct.m
@@ -0,0 +1,28 @@
+function header = emptyHeaderStruct(xsize,xprecision,xcomplex,xdistributed)
+    header = struct();
+    dims = length(xsize);
+
+    header.dims = dims;
+    header.size = xsize;
+    header.offset = zeros(1,dims);
+    header.interval = ones(1,dims);
+
+    header.precision = xprecision;
+    header.complex = xcomplex;
+
+    for i=1:dims % units
+        u(i) = {['u',int2str(i)]};
+    end
+    header.unit = u;
+    for i=1:dims % labels
+        l(i) = {['l',int2str(i)]};
+    end
+    header.label = l;
+
+    header.distributed = xdistributed;
+    header.distribution = struct();
+    header.distribution.dim = nan;
+    header.distribution.distribution = nan;
+    header.distribution.location =nan;
+
+end
