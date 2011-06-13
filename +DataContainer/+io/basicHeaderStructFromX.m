1864116261e111864f04f3f0a927b5bdf1cf08af
Henryk Modzelewski <henryk_modzelewski@mac.com>
2011-06-13 12:52:49 -0700

    added basicHeaderStructFromX.m

---
+++ +DataContainer/+io/basicHeaderStructFromX.m
@@ -0,0 +1,32 @@
+function header = basicHeaderStructFromX(x)
+    header = struct();
+    xsize = size(x);
+    xprecision = DataContainer.utils.getPrecision(x);
+    xcomplex = ~isreal(x);
+    xdistributed = isdistributed(x);
+    dims = length(xsize);
+
+    header.dims = dims;
+    header.size = xsize;
+    header.offset = zeros(1,dims);
+    header.interval = ones(1,dims);
+
+    header.precision = xprecision;
+    header.complex = xcomplex;
+
+    for i=1:dims % units
+        u(i) = {['u',int2str(i)]};
+    end
+    header.unit = u;
+    for i=1:dims % labels
+        l(i) = {['l',int2str(i)]};
+    end
+    header.label = l;
+
+    header.distributed = xdistributed;
+    header.distribution = struct();
+    header.distribution.dim = nan;
+    header.distribution.distribution = nan;
+    header.distribution.location =nan;
+
+end
