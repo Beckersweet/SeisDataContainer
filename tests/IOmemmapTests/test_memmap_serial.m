a8dd07a0c133636741033c01075db15499f1ec3f
Siavash Jalali <svshjalali@gmail.com>
2011-06-28 10:34:59 -0700

    test_serial_basicHeaderStruct

--- tests/IOmemmapTests/test_memmap_serial.m
+++ tests/IOmemmapTests/test_memmap_serial.m
@@ -2,6 +2,14 @@ function test_suite = test_memmap_serial
 initTestSuite;
 end
 
+function test_serial_basicHeaderStruct
+%%
+    imat=rand(13,11,9);
+    hdrx=DataContainer.io.basicHeaderStructFromX(imat);
+    hdrb=DataContainer.io.basicHeaderStruct(hdrx.size,hdrx.precision,hdrx.complex);
+    assert(isequal(hdrx,hdrb))
+end
+
 function test_serial_file_single_complex
 %%
     imat=rand(13,11,9);
