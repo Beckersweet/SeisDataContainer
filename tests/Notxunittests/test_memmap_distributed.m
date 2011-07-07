409b1af06791a6c62cb0067233b55db5335fb1b3
Henryk Modzelewski <henryk_modzelewski@mac.com>
2011-07-07 10:59:53 -0700

    minor changes to tests/Notxunittests/test_memmap_distributed.m

--- tests/Notxunittests/test_memmap_distributed.m
+++ tests/Notxunittests/test_memmap_distributed.m
@@ -2,52 +2,55 @@ function test_distributed()
 assert(matlabpool('size')>0,'matlabpool has to be open first')
 disp('Start');
 I=13; J=11; K=9;
-I=2; J=2; K=4;
-imat=distributed.rand(I,J,K);
-imat=complex(imat,imat);
+I=2; J=matlabpool('size')*1; K=matlabpool('size')*2;
+imat3=distributed.rand(I,J,K);
+imat3=complex(imat3,imat3);
+spmd
+    codistr = codistributor1d(2,codistributor1d.unsetPartition,[I J K]);
+    myLocalPart = rand(I,1,K);
+    imat2 = codistributed.build(myLocalPart, codistr);
+end
 td=DataContainer.io.makeDir()
 
-hdrb=DataContainer.io.basicHeaderStructFromX(imat);
-hdrx=DataContainer.io.addDistHeaderStructFromX(imat,hdrb);
+hdrb=DataContainer.io.basicHeaderStructFromX(imat3);
+hdrx=DataContainer.io.addDistHeaderStructFromX(imat3,hdrb);
 hdrd=DataContainer.io.addDistHeaderStruct(hdrx.distribution.dim,[],hdrb);
 assert(isequal(hdrx,hdrd),'distributions do not match')
 
 disp('serial')
-DataContainer.io.memmap.dist.FileWrite(td,imat,0);
+DataContainer.io.memmap.dist.FileWrite(td,imat3,0);
 [x hdrn] = DataContainer.io.memmap.dist.FileRead(td);
 for k=1:K
     x = DataContainer.io.memmap.dist.FileReadLeftSlice(td,[k]);
-    assert(isequal(x,imat(:,:,k)),'no match')
+    assert(isequal(x,imat3(:,:,k)),'no match')
 end
 ls('-lR',td)
 DataContainer.io.memmap.serial.FileDelete(td);
 
-hdrs=DataContainer.io.basicHeaderStructFromX(imat);
+hdrs=DataContainer.io.basicHeaderStructFromX(imat3);
 DataContainer.io.memmap.serial.FileAlloc(td,hdrs);
 for k=1:K
-    mat = distributed.rand(I,J);
-    DataContainer.io.memmap.dist.FileWriteLeftSlice(td,mat,[k])
+    DataContainer.io.memmap.dist.FileWriteLeftSlice(td,imat2(:,:,k),[k])
     x = DataContainer.io.memmap.dist.FileReadLeftSlice(td,[k]);
-    assert(isequal(mat,x))
+    assert(isequal(imat2(:,:,k),x))
 end
 ls('-lR',td)
 DataContainer.io.memmap.serial.FileDelete(td);
 
 disp('distributed')
-DataContainer.io.memmap.dist.FileWrite(td,imat,1);
+DataContainer.io.memmap.dist.FileWrite(td,imat3,1);
 [x hdrn] = DataContainer.io.memmap.dist.FileRead(td);
 ls('-lR',td)
 DataContainer.io.memmap.dist.FileDelete(td);
 
-hdrs=DataContainer.io.basicHeaderStructFromX(imat);
+hdrs=DataContainer.io.basicHeaderStructFromX(imat3);
 hdrs=DataContainer.io.addDistHeaderStruct(hdrs.dims-1,[],hdrs);
 hdrs=DataContainer.io.addDistFileHeaderStruct(td,hdrs);
 DataContainer.io.memmap.dist.FileAlloc(td,hdrs);
 for k=1:K
-    mat = distributed.rand(I,J);
-    DataContainer.io.memmap.dist.FileWriteLeftSlice(td,mat,[k])
+    DataContainer.io.memmap.dist.FileWriteLeftSlice(td,imat2(:,:,k),[k])
     x = DataContainer.io.memmap.dist.FileReadLeftSlice(td,[k]);
-    assert(isequal(mat,x))
+    assert(isequal(imat2(:,:,k),x))
 end
 ls('-lR',td)
 DataContainer.io.memmap.dist.FileDelete(td);
