57ff415fee7dd377a5215f57fd0ab87624213bc8
Thomas <yuyanglai@yahoo.com>
2011-07-18 10:13:11 -0700

    WIP on memoCon constructor

--- @memoCon/memoCon.m
+++ @memoCon/memoCon.m
@@ -1,13 +1,18 @@
 classdef memoCon < outCon
     %MEMOCON  Memory-mapping out-of-core data container for binaries
     %
-    %   memoCon(FILENAME,SIZE,PARAM1,VALUE1,...)
+    %   memoCon(DIRNAME,FILENAME,SIZE,PARAM1,VALUE1,...)
+    %
+    %   Parameters:
+    %   offset, precision, repeat.
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %   PROPERTIES
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     properties (SetAccess = protected)
-        filename = ''; % Filename of data        
+        dirname    = ''; % Directory name of data
+        filename   = ''; % Filename of data
+        prec;
         
     end % properties
     
@@ -15,17 +20,43 @@ classdef memoCon < outCon
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         %   Constructor
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        function x = memoCon(fname,varargin)
+        function x = memoCon(dname,fname,varargin)
             % Constructor for memoCon
             % Process input arguments
+            assert(ischar(dname), 'filename must be a string')
             assert(ischar(fname), 'filename must be a string')
             
-            % Reads in data and convert to intermediate format
+            % Setup parameters            
+            offset     = 0;
+            precision  = 'double';
+            repeat;
+            dimensions = [];
+            
+            % Parse param-value pairs
+            for i = 1:2:length(varargin)
+
+                assert(ischar(varargin{i}),...
+                    'Parameter at input %d must be a string.', i);
+
+                fieldname = lower(varargin{i});
+                switch fieldname
+                    case {'offset', 'precision', 'repeat', 'dimensions'}
+                        eval([fieldname ' = varargin{i+1};']);
+                    otherwise
+                        error('Parameter "%s" is unrecognized.', ...
+                            varargin{i});
+                end
+            end
+            
+            % Reads in data and convert to intermediate format? or maybe
+            % just read in data header
             
             % Construct and set class attributes
-            x = x@outCon(type,dims,iscomplex);
-            x.exdims = 0;
-            x.imdims = 0;
+            x = x@outCon('serial memmap',dimensions,iscomplex);
+            x.dirname   = dname;
+            x.filename  = fname;
+            x.exdims    = 0; % Explicit dimensions of data
+            x.imdims    = 0;
             x.iscomplex = false;
         end % constructor
         
