998b21c68b0bd4c80327b217406c6d320bb6566f
Siavash Jalali <svshjalali@gmail.com>
2011-08-30 17:14:34 -0700

    assertEqual for oMatCon

---
+++ @oMatCon/assertEqual.m
@@ -0,0 +1,41 @@
+function assertEqual(a,b)
+    if(~isa(a,'oCon') && ~isa(b,'iCon'))
+        error('both parameters should be data containers')
+    end
+    
+    if(~isequal(a.dimensions,b.dimensions))
+        error('sizes does not match')
+    end
+    
+    global SDCbufferSize;
+    % Set byte size
+    bytesize  = DataContainer.utils.getByteSize(a.header.precision);
+    
+    % Set the sizes
+    dims      = [1 prod(a.dimensions)];
+    reminder  = prod(a.dimensions);
+    maxbuffer = SDCbufferSize/bytesize;    
+    rstart = 1;
+    
+    while (reminder > 0)
+        buffer = min(reminder,maxbuffer);
+        rend = rstart + buffer - 1;
+        r1 = DataContainer.io.memmap.serial.DataReadLeftChunk...
+            (a.dirname,'real',dims,[rstart rend],[],a.header.precision,a.header.precision);
+        if a.header.complex
+        dummy = DataContainer.io.memmap.serial.DataReadLeftChunk...
+            (a.dirname,'imag',dims,[rstart rend],[],a.header.precision,a.header.precision);
+            r1 = complex(r,dummy);
+        end
+        r2 = DataContainer.io.memmap.serial.DataReadLeftChunk...
+            (b.dirname,'real',dims,[rstart rend],[],b.header.precision,b.header.precision);
+        if b.header.complex
+        dummy = DataContainer.io.memmap.serial.DataReadLeftChunk...
+            (b.dirname,'imag',dims,[rstart rend],[],b.header.precision,b.header.precision);
+            r2 = complex(r,dummy);
+        end
+        assert(isequal(r1,r2),'The datacontainers are not equal')
+        reminder = reminder - buffer;
+        rstart   = rend + 1;
+    end
+end
--- tests/test_oMatCon.m
+++ tests/test_oMatCon.m
@@ -1,3 +1,23 @@
 function test_suite = test_oMatCon
-initTestSuite;
+    initTestSuite;
 end
+
+function test_oMatCon_norm
+%% norm
+y = oMatCon.randn(3,3,3);
+x(:,:,1) = y(:,1:3,1);
+x(:,:,2) = y(:,1:3,2);
+x(:,:,3) = y(:,1:3,3);
+assertElementsAlmostEqual(y.norm(0),norm(vec(x),0));
+assertElementsAlmostEqual(y.norm(1),norm(vec(x),1));
+assertElementsAlmostEqual(y.norm(2),norm(vec(x),2));
+assertElementsAlmostEqual(y.norm(inf),norm(vec(x),inf));
+assertElementsAlmostEqual(y.norm(-inf),norm(vec(x),-inf));
+assertElementsAlmostEqual(y.norm('fro'),norm(vec(x),'fro'));
+end % norm
+
+function test_oMatCon_assertEqual
+%% norm
+y = oMatCon.randn(3,3,3);
+x = oMatCon.randn(3,3,3);
+end % norm
