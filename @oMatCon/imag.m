02e48cb3bf5f4c54c96b1e92ce1bb97894d04d6b
Siavash Jalali <svshjalali@gmail.com>
2011-09-15 13:05:36 -0700

    several fixes and comments for oMatCon

--- @oMatCon/imag.m
+++ @oMatCon/imag.m
@@ -1,37 +1,13 @@
 function y = imag(a)
-    if(~isa(a,'oCon'))
-        error('parameter should be data container')
+    if(~isa(a,'oMatCon'))
+        error('Input parameter should be data container')
     end
-    
-    global SDCbufferSize;
-    % Set byte size
-    bytesize  = DataContainer.utils.getByteSize(a.header.precision);
-    
-    y = oMatCon.zeros(a.dimensions);
-    header = a.header;
-    header.size = [1 prod(a.header.size)];
-    DataContainer.io.memmap.serial.HeaderWrite(y.dirname,header);    
-    
-    % Set the sizes
-    dims      = [1 prod(a.dimensions)];
-    reminder  = prod(a.dimensions);
-    maxbuffer = SDCbufferSize/bytesize;    
-    rstart = 1;
-    
-    while (reminder > 0)
-        buffer = min(reminder,maxbuffer);
-        rend = rstart + buffer - 1;
-        r1 = DataContainer.io.memmap.serial.DataReadLeftChunk...
-            (a.dirname,'real',dims,[rstart rend],[],a.header.precision,a.header.precision);
-        if a.header.complex
-        dummy = DataContainer.io.memmap.serial.DataReadLeftChunk...
-            (a.dirname,'imag',dims,[rstart rend],[],a.header.precision,a.header.precision);
-            r1 = complex(r1,dummy);
-        end        
-        DataContainer.io.memmap.serial.FileWriteLeftChunk...
-            (y.dirname,imag(r1),[rstart rend],[]);
-        reminder = reminder - buffer;
-        rstart   = rend + 1;
+    if(~a.header.complex)
+        error('Epic fail: The dataContainer is not complex')
     end
-    DataContainer.io.memmap.serial.HeaderWrite(y.dirname,a.header);
+    y = oMatCon.zeros(a.dimensions); 
+    copyfile([a.dirname filesep 'imag'],y.dirname)
+    headerOut = DataContainer.io.memmap.serial.HeaderRead(y.dirname);
+    headerOut.complex = 1;
+    DataContainer.io.memmap.serial.HeaderWrite(y.dirname,headerOut);
 end
