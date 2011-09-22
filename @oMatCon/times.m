4792d899f13a6f59af16b2acd3cd793f50d7888c
Siavash Jalali <svshjalali@gmail.com>
2011-09-22 10:59:35 -0700

    FileTimes serial tool and times for oMatCon

---
+++ @oMatCon/times.m
@@ -0,0 +1,13 @@
+function y = times(A,B)
+if(isa(A,'oMatCon'))
+        A = A.dirname;
+    end
+    if(isa(B,'oMatCon'))
+        B = B.dirname;
+    end
+    td = DataContainer.io.makeDir();
+    DataContainer.io.memmap.serial.FileTimes...
+        (A,B,td);
+    y  = oMatCon.load(td);
+end
+
