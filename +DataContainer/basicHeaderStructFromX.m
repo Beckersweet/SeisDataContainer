20b91e0873c80ee75eaa0c4a6fec71f41bac3aa2
Henryk Modzelewski <henryk_modzelewski@mac.com>
2011-11-17 15:39:00 -0800

    added parameters to +DataContainer/basicHeaderStruct*

--- +DataContainer/basicHeaderStructFromX.m
+++ +DataContainer/basicHeaderStructFromX.m
@@ -1,4 +1,20 @@
-function header = basicHeaderStructFromX(x)
+function header = basicHeaderStructFromX(x,varargin)
+% basicHeaderStructFromX creates the DataContainer header from MATLAB array X
+%
+%   Required argumens:
+%       X - MATLAB array
+%
+%   Optional keyword arguments:
+%       'variable' - string holding the name of variable
+%       'origin' - row vector holding the origin coordinate for ach axis
+%       'delta' - row vector holding the delta of coordinate value for each axis
+%       'unit' - cell of sctrings with units for each axis
+%       'label' - cell of strings with label for each axis
+%
+%   EXAMPLE:
+%       DataContainer.basicHeaderStructFromX(rand(10),...
+%           'variable','test','origin',[1 2],'delta',[7 7],'unit',{'m','m'},'label',{'x','y'})
+%
     header = struct();
     xsize = size(x);
     xprecision = DataContainer.utils.getPrecision(x);
@@ -24,5 +40,19 @@ function header = basicHeaderStructFromX(x)
     header.label = l;
 
     header.distributed = 0;
+
+    p = inputParser;
+    p.addParamValue('variable',header.variable,@ischar);
+    p.addParamValue('origin',header.origin,@(x)isrow(x)&&length(x)==dims);
+    p.addParamValue('delta',header.delta,@(x)isrow(x)&&length(x)==dims);
+    p.addParamValue('unit',header.unit,@(x)iscell(x)&&length(x)==dims);
+    p.addParamValue('label',header.label,@(x)iscell(x)&&length(x)==dims);
+    p.parse(varargin{:});
+
+    header.variable = p.Results.variable;
+    header.origin = p.Results.origin;
+    header.delta = p.Results.delta;
+    header.unit = p.Results.unit;
+    header.label = p.Results.label;
 
 end
