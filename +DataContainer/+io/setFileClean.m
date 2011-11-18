a3bd543932269dc17a54c928c6185f6204379d2f
Henryk Modzelewski <henryk_modzelewski@mac.com>
2011-11-18 14:54:53 -0800

    added isFileClean.m

--- +DataContainer/+io/setFileClean.m
+++ +DataContainer/+io/setFileClean.m
@@ -1,10 +1,11 @@
-function status = setClean(dirname,varargin)
+function setFileClean(dirname,varargin)
     assert(ischar(dirname),'dirname must be a string')
+    assert(isdir(dirname),'dirname %s is not a directory or does not exist',dirname)
     lockname = '_IOinProgress';
 
     error(nargchk(1, 2, nargin, 'struct'));
     if nargin > 1
-        assert(ischar(varargin{1}),'lock name is not a string');
+        assert(ischar(varargin{1}),'file name is not a string');
         lockname = varargin{1};
     end
 
