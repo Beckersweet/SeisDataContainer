c7f27e2a1dac894714062df6b37cc55b97e8e97d
Henryk Modzelewski <henryk_modzelewski@mac.com>
2011-11-22 18:57:57 -0800

    create directory in save method

--- @iCon/save.m
+++ @iCon/save.m
@@ -3,6 +3,9 @@ function save(obj,dirname)
 
     header      = obj.header;
     header.size = cell2mat(header.size);
+    assert(~isdir(dirname),'Fatal error: directory %s already exists',dirname);
+    status = mkdir(dirname);
+    assert(status,'Fatal error while creating directory %s',tmpdir);
     DataContainer.io.memmap.serial.FileWrite(dirname,double(obj));
     DataContainer.io.memmap.serial.HeaderWrite(dirname,header);
 end
--- @piCon/save.m
+++ @piCon/save.m
@@ -2,6 +2,9 @@ function save(obj,dirname)
 %PICON.SAVE Saves piCon to file
     header      = obj.header;
     header.size = cell2mat(header.size);
+    assert(~isdir(dirname),'Fatal error: directory %s already exists',dirname);
+    status = mkdir(dirname);
+    assert(status,'Fatal error while creating directory %s',tmpdir);
     DataContainer.io.memmap.serial.FileWrite(dirname,gather(double(obj)));
     DataContainer.io.memmap.serial.HeaderWrite(dirname,header);
 end
