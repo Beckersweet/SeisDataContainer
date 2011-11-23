bcdaf41ca553c9797a7fd8a79d34e665b7a7a6a6
Henryk Modzelewski <henryk_modzelewski@mac.com>
2011-11-22 19:00:11 -0800

    fix create directory in save method

--- @iCon/save.m
+++ @iCon/save.m
@@ -5,7 +5,7 @@ function save(obj,dirname)
     header.size = cell2mat(header.size);
     assert(~isdir(dirname),'Fatal error: directory %s already exists',dirname);
     status = mkdir(dirname);
-    assert(status,'Fatal error while creating directory %s',tmpdir);
+    assert(status,'Fatal error while creating directory %s',dirname);
     DataContainer.io.memmap.serial.FileWrite(dirname,double(obj));
     DataContainer.io.memmap.serial.HeaderWrite(dirname,header);
 end
