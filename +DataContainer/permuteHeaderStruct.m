1c460c278fabe3d09c15f95e33b602a35893fc9d
Siavash Jalali <svshjalali@gmail.com>
2011-11-30 12:38:04 -0800

    minor for for permute

--- +DataContainer/permuteHeaderStruct.m
+++ +DataContainer/permuteHeaderStruct.m
@@ -2,14 +2,16 @@ function newHeader = permuteHeaderStruct( header,perm )
 %PERMUTEHEADERSTRUCT Permutation for data container header
 %   
 
-header.delta  = permute(header.delta,perm);
-header.origin = permute(header.origin,perm);
-
 temp1 = header.label;
 temp2 = header.unit;
+temp3 = header.delta;
+temp4 = header.origin;
+
 for i=1:length(perm)
-    header.label(i) = temp1(perm(i));
-    header.unit(i)  = temp2(perm(i));
+    header.label(i)  = temp1(perm(i));
+    header.unit(i)   = temp2(perm(i));
+    header.delta(i)  = temp3(perm(i));
+    header.origin(i) = temp4(perm(i));
 end
 
 newHeader = header;
