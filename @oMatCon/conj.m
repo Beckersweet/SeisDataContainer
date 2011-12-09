c19b4b0a63656d30eba7bebb0196dc20845acb9f
Siavash Jalali <svshjalali@gmail.com>
2011-12-09 11:15:37 -0800

    FileConj added to low level utilities, conj for oMatCon modified

--- @oMatCon/conj.m
+++ @oMatCon/conj.m
@@ -1,37 +1,13 @@
 function y = conj(a)
-    global SDCbufferSize;
-    % Set byte size
-    bytesize  = DataContainer.utils.getByteSize(precision(a));
-    y = oMatCon.zeros(size(a));
-    header = a.header;
-    header.complex = 1;
-    DataContainer.io.memmap.serial.FileAlloc(path(y.pathname),header);
-    header.size = [1 prod(size(a))];
-    DataContainer.io.memmap.serial.HeaderWrite(path(y.pathname),header);
-    
-    % Set the sizes
-    sizez      = [1 prod(size(a))];
-    reminder  = prod(size(a));
-    maxbuffer = SDCbufferSize/bytesize;    
-    rstart = 1;
-    
-    while (reminder > 0)
-        buffer = min(reminder,maxbuffer);
-        rend = rstart + buffer - 1;
-        r1 = DataContainer.io.memmap.serial.DataReadLeftChunk...
-            (path(a.pathname),'real',sizez,[rstart rend],[],precision(a),precision(a));
-        if ~isreal(a)
-        dummy = DataContainer.io.memmap.serial.DataReadLeftChunk...
-            (path(a.pathname),'imag',sizez,[rstart rend],[],precision(a),precision(a));
-            r1 = complex(r1,dummy);
-        end
-        DataContainer.io.memmap.serial.FileWriteLeftChunk...
-            (path(y.pathname),conj(r1),[rstart rend],[]);
-        reminder = reminder - buffer;
-        rstart   = rend + 1;
-    end
-    header = a.header;
-    header.complex = 1;
-    DataContainer.io.memmap.serial.HeaderWrite(path(y.pathname),header);
-    y.header.complex = 1;
+%CONJ
+%
+%   conj(A) Creates a new oMatCon which is the complex conjugate of input
+%   oMatCon a
+%
+%   A  - Input oMatCon
+%
+aa = path(a.pathname);
+td = ConDir();    
+DataContainer.io.memmap.serial.FileConj(aa,path(td));
+y  = oMatCon.load(td);
 end
