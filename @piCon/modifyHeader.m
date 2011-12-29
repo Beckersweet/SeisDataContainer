763063091b1259073b0d654870609dfae0a69c76
Thomas <yuyanglai@yahoo.com>
2011-12-29 15:59:06 -0800

    Fixed piCon/modifyHeader, datacontest passed

--- @piCon/modifyHeader.m
+++ @piCon/modifyHeader.m
@@ -1,5 +1,25 @@
-function x = modifyHeader(obj,varargin)
+function y = modifyHeader(obj,varargin)
 %MODIFYHEADER Modifies the header attributes and keeps the other header
 %fields untouched
-x = {piCon(double(obj),'header',obj.header,varargin{:}{:})};
+
+y      = obj;
+header = y.header;
+dims   = y.header.dims;
+p      = inputParser;
+p.addParamValue('varName',header.varName,@ischar);
+p.addParamValue('varUnits',header.varUnits,@ischar);
+p.addParamValue('origin',header.origin,@(x)isrow(x)&&length(x)==dims);
+p.addParamValue('delta',header.delta,@(x)isrow(x)&&length(x)==dims);
+p.addParamValue('unit',header.unit,@(x)iscell(x)&&length(x)==dims);
+p.addParamValue('label',header.label,@(x)iscell(x)&&length(x)==dims);
+p.parse(varargin{:}{:});
+
+header.varName  = p.Results.varName;
+header.varUnits = p.Results.varUnits;
+header.origin   = p.Results.origin;
+header.delta    = p.Results.delta;
+header.unit     = p.Results.unit;
+header.label    = p.Results.label;
+y.header        = header;
+y               = {y};
 end
