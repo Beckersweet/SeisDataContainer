151c10c23746a8b69f4aafd02675cb5c4c8e36f4
Thomas <yuyanglai@yahoo.com>
2012-03-08 16:39:11 -0800

    WIP on subsrefhelper

--- @iCon/subsrefHelper.m
+++ @iCon/subsrefHelper.m
@@ -4,22 +4,17 @@ function y = subsrefHelper(x,s)
 %   Interface that handles the propagation of header data in datacon.
 %   Nothing the user needs to know here.
 %
-%
 
 % Checking indices
-indices  = s.subs;
-dimslist = {1:sum(size(x))};
-if length(indices) == 1 && indices{1} == ':' % vec case
-    y = vec(x);
-elseif
+% We need to extract a vectors of indices for use in header subsref.
+subs  = s.subs;
+indices = zeros(1,dims(x));
+if length(subs) == dims(x) % Number of indices is same as dimensions.
     
+elseif subs{end} == ':'
+        
 else
-    
-end
 
-for i=1:length(tempsubs) && length(tempsubs) > 1
-    assert(~strcmp(tempsubs{i},':'),...
-        'Colon indexing only allowed on contiguous fast dimensions');
 end
 
 % Data processing
@@ -28,9 +23,6 @@ data = subsref(x.data,s);
 % Header processing
 xheader   = x.header;
 neworigin = xheader.origin;
-% for j = 1:length(indices)
-%     neworigin(j) = xheader.origin(j) + (indices{j}-1)*xheader.delta(j);
-% end
 
 % Repackage and export
 y                      = construct(x,data);
