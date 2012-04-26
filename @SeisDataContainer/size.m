6dc2bc582bcba70b882e1121889eaf69b4e41cfd
Thomas <yuyanglai@yahoo.com>
2012-04-26 14:03:56 -0700

    Fixed size function in SDC to reflect the purity of explicivity of the function

--- @SeisDataContainer/size.m
+++ @SeisDataContainer/size.m
@@ -1,17 +1,3 @@
-function y = size(x,dim)
-    if(nargin == 2)
-        if(isequal(class(x.header.size),'cell'))
-            y = x.header.size(dim);
-            y = cell2mat(y);
-        else
-            y = x.exsize(dim);
-        end
-    else
-        if(isequal(class(x.header.size),'cell'))
-            y = x.header.size;
-            y = cell2mat(y);
-        else
-            y = x.exsize;
-        end
-    end
+function y = size(x,varargin)
+    y = x.exsize(varargin{:});
 end
