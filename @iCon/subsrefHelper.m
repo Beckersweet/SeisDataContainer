679850639b52a19b194917d1f35caed5efda7a28
Thomas <yuyanglai@yahoo.com>
2012-04-27 12:41:55 -0700

    Added index skipping check for subsrefhelper

--- @iCon/subsrefHelper.m
+++ @iCon/subsrefHelper.m
@@ -5,28 +5,16 @@ function y = subsrefHelper(x,s)
 %   Nothing the user needs to know here.
 %
 
-% % Checking indices
-% % We need to extract a vectors of indices for use in header subsref.
-% ind = zeros(dims(x),2); % Start and end indices for each dimension
-% if length(s.subs) == 1 && s.subs{end} == ':' % Vectorizing case
-%     for i = 1:dims(x)
-%         ind(i,1) = 1;
-%         ind(i,2) = size(x,i);
-%     end
-% elseif length(s.subs) <= dims(x) && s.subs{end} ~= ':' % No right fills
-%     Number of left dimensions.
-%     for i = 1:length(s.subs)
-%         if s.subs{i} == ':'
-%             ind(i,1) = 1;
-%             ind(i,2) = size(x,i);
-%         else
-%             ind(i,1) = s.subs{i}(1);
-%             ind(i,2) = s.subs{i}(end);
-%         end
-%     end
-% else
-%     error('This kind of indexing is not supported');
-% end
+% Checking indices
+% We need to extract a vectors of indices for use in header subsref.
+if length(s.subs) == 1 && s.subs{end} == ':' % Vectorizing case
+    
+else % multiple dims case
+    for i = 1:length(s.subs)
+        assert(length(s.subs{i}) == s.subs{i}(end)-s.subs{i}(1) + 1,...
+            'Index skipping is not allowed');
+    end
+end
 
 % Data processing
 data = subsref(x.data,s);
