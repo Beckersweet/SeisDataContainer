31d9f78092645b7b55f4388232ba862eaa76d044
Thomas <yuyanglai@yahoo.com>
2012-05-22 11:30:17 -0700

    fixed metacopy

--- @iCon/metacopy.m
+++ @iCon/metacopy.m
@@ -4,5 +4,6 @@ function y = metacopy(x,y)
 %   y = metacopy(x,y) will copy the metadata of data container x into y and
 %   return y.
 
+y.exsize = x.exsize;
 y.perm   = x.perm;
 y.strict = x.strict;
