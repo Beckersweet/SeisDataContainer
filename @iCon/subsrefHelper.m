3ed1baae7ac51d86e25e8971fe0009ae036a4fc2
Thomas <yuyanglai@yahoo.com>
2012-05-30 18:04:09 -0700

    bugfix for subsrefHelper header indexing

--- @iCon/subsrefHelper.m
+++ @iCon/subsrefHelper.m
@@ -5,13 +5,15 @@ function y = subsrefHelper(x,s)
 %   Nothing the user needs to know here.
 %
 
+lastdim = max(2,length(s.subs));
+
 % Checking indices
 % We need to extract a vector of indices for use in header subsref.
 if length(s.subs) == 1 && all(s.subs{:} == ':') % Vectorizing case
     y = vec(x);
     return;
-else % multiple dims case
     
+else % multiple dims case    
     % indexing must be same # of dimensions as x
     assert(length(s.subs)==size(x.exsize,2),...
         'index must have the same ndims as the datacontainer')
@@ -26,23 +28,26 @@ else % multiple dims case
             assert(~vectorized && ~colonized,...
                 'May only use scalars to index over rightmost dimensions')
             
+            lastdim = max(2,lastdim - 1);
+            
         % case vector a:b
         elseif isvector(s.subs{i}) && isnumeric(s.subs{i})
             assert(~colonized,...
                 'Colon indexing cannot be used to the right of vectors');
             
-            a = s.subs{i}(1);
-            b = s.subs{i}(end);
-            assert(length(s.subs{i}) == b - a + 1,...
-                'Index skipping is not allowed');
+            a     = s.subs{i}(1);
+            b     = s.subs{i}(end);
             xdims = x.exsize(:,i);
             xdims = xdims';
             
+            % Check for index skipping
+            assert(length(s.subs{i}) == b - a + 1,...
+                'Index skipping is not allowed');
             
+            % a & b must mod cleanly with the products of faster dimensions
             for j=1:length(xdims)-1
-                c = x.header.size(xdims(j));
-                assert(mod(a,c)==1 &&...
-                       mod(b,c)==0,...
+                c = prod(x.header.size(1:xdims(j)));
+                assert(mod(a,c)==1 && mod(b,c)==0,...
                     'Cross-dimensional indexing not allowed');           
             end
             
@@ -56,10 +61,8 @@ else % multiple dims case
         % What??? I don't even...
         else
             error(['Unrecognized indexing' s.subs{i}]);
-        end
-        
-    end
-    
+        end        
+    end    
 end
 
 % Data processing
@@ -67,10 +70,10 @@ data = subsref(x.data,s);
 
 % Repackage and export
 y                      = construct(x,data);
-y.header.delta         = x.header.delta(1:dims(y));
-y.header.origin        = x.header.origin(1:dims(y));
+y.header.delta         = x.header.delta(1:lastdim);
+y.header.origin        = x.header.origin(1:lastdim);
 y.header.precision     = x.header.precision;
 y.header.complex       = x.header.complex;
-y.header.unit          = x.header.unit(1:dims(y));
-y.header.label         = x.header.label(1:dims(y));
+y.header.unit          = x.header.unit(1:lastdim);
+y.header.label         = x.header.label(1:lastdim);
 y.header.distributedIO = x.header.distributedIO;
