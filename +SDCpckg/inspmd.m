623a986ac3f9d7015d9a7aedb5bc85b78ce9c055
Henryk Modzelewski <henryk_modzelewski@mac.com>
2012-06-20 15:47:35 -0700

    fixed missing ;

--- +SDCpckg/inspmd.m
+++ +SDCpckg/inspmd.m
@@ -1,4 +1,4 @@
 function flag = inspmd()
     flag = matlabpool('size')==0;
-    flag = flag && ~isempty(com.mathworks.toolbox.distcomp.pmode.SessionFactory.getCurrentSession)
+    flag = flag && ~isempty(com.mathworks.toolbox.distcomp.pmode.SessionFactory.getCurrentSession);
 end
