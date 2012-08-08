150ee4bc7006bc26dd9ea702c6d0c52032896130
Barbara Collignon <barbara.collignon@gmail.com>
2012-08-08 15:53:30 -0700

    HeaderWrite.m created

---
+++ +SDCpckg/+io/+JavaSeis/+serial/HeaderWrite.m
@@ -0,0 +1,16 @@
+function HeaderWrite(dirname,x)
+%HEADERWRITE writes header
+
+% %Write file
+% DataContainer.io.memmap.serial.DataAlloc(dirname,'real',header.size,header.precision);
+% if header.complex
+%     DataContainer.io.memmap.serial.DataAlloc(dirname,'imag',header.size,header.precision);
+% end
+% %Write header
+% DataContainer.io.memmap.serial.HeaderWrite(dirname,header);
+
+
+
+
+
+end
