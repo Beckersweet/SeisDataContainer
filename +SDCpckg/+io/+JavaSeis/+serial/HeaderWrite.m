a5780bd9ed5d45ef5b9d9c60f0ba34703ab19270
Barbara Collignon <barbara.collignon@gmail.com>
2012-08-10 16:53:46 -0700

    New comment

--- +SDCpckg/+io/+JavaSeis/+serial/HeaderWrite.m
+++ +SDCpckg/+io/+JavaSeis/+serial/HeaderWrite.m
@@ -1,5 +1,6 @@
 function header = HeaderWrite(x,xprecision,xcomplex)
 %HEADERWRITE creates Header as MATLAB Struct
+%TESTED with header = HeaderWrite([250,30,100,10],'double',0)
 
 % Import package
 import SDCpckg.* 
