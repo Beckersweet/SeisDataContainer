53d6b64cf7d43760d65ddefdd27f62ff9fa11589
Barbara Collignon <barbara.collignon@gmail.com>
2012-08-20 14:22:12 -0700

    New comment

--- +SDCpckg/+io/+JavaSeis/+serial/FileWrite.m
+++ +SDCpckg/+io/+JavaSeis/+serial/FileWrite.m
@@ -82,8 +82,8 @@ y = ones(AxisLengths') ;
 formatgridsize = size(y) ;
 
 
-
-  % if less than 4D: Reshape to 4D
+ 
+% if less than 4D: Reshape to 4D
   if length(formatgridsize) < 4
      for nullDim=length(formatgridsize):3
   
@@ -139,12 +139,13 @@ for hyp=1:1
            
 end
 
-   position(1) = 0;
-   position(2) = 0;
-   position(3) = 0;
-   position(4) = 0; 
+    position(1) = 0;
+    position(2) = 0;
+    position(3) = 0;
+    position(4) = 0;
+   %seisio.setPosition(position); 
    seisio.writeMultiArray(grid_multiarray,position) ;
-   % Null pointer Exeption line 409 in beta.javaseis.array.BigArrayJava1D.putArray:System.arraycopy(..source_array...dest_array..)
+   % Null pointer Exeption line 409 in beta.javaseis.array.BigArrayJava1D.getArray:System.arraycopy(..source_array...dest_array..)
            % The destination array "dest_array" has not been allocated
            % Need to use Abstract function:setLength to allocate "dest_array"
            % setLength in IBigArray is overriden in BigArrayJava1D
