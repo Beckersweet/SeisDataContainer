a2033f31ba694b78384789b8a56108776ad50554
Barbara Collignon <barbara.collignon@gmail.com>
2012-09-09 19:30:26 -0700

    ReadLeftChunk in up to 4D slice

--- +SDCpckg/+io/+JavaSeis/+serial/FileReadLeftChunk.m
+++ +SDCpckg/+io/+JavaSeis/+serial/FileReadLeftChunk.m
@@ -1,14 +1,15 @@
 function [x header] = FileReadLeftChunk(dirname,range,slice,varargin)
 %FILEREADLEFTCHUNK Reads serial left chunck data from binary file
 %
-% Edited for JavaSeis by Trisha
+% Edited for JavaSeis by Trisha, Barbara
 %
 %   [X, HEADER] = FileReadLeftChunk(DIRNAME,RANGE,SLICE,X_PRECISION) reads
 %   the serial left chunk from DIRNAME/FILENAME.
 %
 %   DIRNAME     - A string specifying the directory name
 %   RANGE       - A vector with two elements specifying the range of data
-%   SLICE       - A vector specifying the slice index
+%   SLICE       - A vector specifying the slice index : slice(1) =  i -
+%   slice(2) = j
 %   X_PRECISION - An optional string specifying the precision of one unit of data,
 %                 defaults to 'double' (8 bits)
 %                 Supported precisions: 'double', 'single'
@@ -34,27 +35,41 @@ seisio = beta.javaseis.io.Seisio( dirname );
 seisio.open('r');
 
 % Read header
-%header = DataContainer.io.javaseis.serial.HeaderRead(dirname);
+header = SDCpckg.io.JavaSeis.serial.HeaderRead(dirname)
 
 % Get number of dimensions and set position accordingly
-dimensions = seisio.getGridDefinition.getNumDimensions();
-assert(isequal(slice,[]), 'Code only completed for slice == []');
-asser(isequal(dimensions, 3), 'Code only completed for 3 dimensions');
+dimensions = header.dims ;
 position = zeros(dimensions,1);
 
-% need to pre-set X to be 3d array of zeros with the correct dimensions
-rangeCount=range(2)-range(1)+1;
-x=zeros(seisio.getGridDefinition.getNumSamplesPerTrace(),...
-    seisio.getGridDefinition.getNumTracesPerFrame(), rangeCount);
+% Get Shape
+shape = header.size ;
+
+%assert(isequal(slice,[]), 'Code only completed for slice == []');
+%asser(isequal(dimensions, 3), 'Code only completed for 3 dimensions');
+position = zeros(dimensions,1);
+
+% Pre-set X to be 4d array of zeros with the correct dimensions
+rangeCount=range(2)-range(1)+1; 
+x = zeros(rangeCount,shape(2),slice(1),slice(2)) ;
+sx = zeros(shape(1),shape(2),slice(1),slice(2)) ;
+
 %x=zeros(226,676,rangeCount);
     % Matlab reads the frame in transposed, so traces then samples. This is
     % an issue to keep in mind. 
-for i = range(1)+1:range(2)+1
-    position(dimensions)=i-1;
+
+% Read up to 4D datasets    
+for j= slice(2):slice(2)
+    position(4) = j-1
+  for i = slice(1):slice(1)
+    position(3) = i - 1 ; 
     seisio.readFrame(position); % reads one 2D "Frame"
-    x(:,:,i) = seisio.getTraceDataArray()'; 
-end
-
-
+    sx(:,:,i,j) = seisio.getTraceDataArray()' ;
+    x = sx(range(1):range(2),:,i,j) ;
+         
+ 
+  end
+end  
+  seisio.close();
  
 end
+
