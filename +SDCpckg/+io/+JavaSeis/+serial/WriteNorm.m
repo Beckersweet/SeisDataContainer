394369c9afb2dd41fc5b9082d03c4a7ccddad25b
Barbara Collignon <barbara.collignon@gmail.com>
2012-09-14 18:42:21 -0700

    JS Calls
    TEST Case - Intial Algo definition

--- +SDCpckg/+io/+JavaSeis/+serial/WriteNorm.m
+++ +SDCpckg/+io/+JavaSeis/+serial/WriteNorm.m
@@ -3,11 +3,15 @@ function OOCNorm = WriteNorm(dirname,x)
 % Load dynamic libraries
  javaaddpath('/Users/bcollignon/Documents/workspace/dhale-jtk-78bca79.jar');
  javaaddpath('/Users/bcollignon/Documents/workspace/betajavaseis1819.jar');
-
+ javaaddpath('/Users/bcollignon/Documents/workspace/jama-1.0.2.jar');
+ 
 
 % Import External Java functions
 % import java.io.RandomAccessFile.* ;
-
+% import edu.mines.jtk.lapack.* ;
+% import jama.* ;
+% L = import
+ 
 % Make Directory
 %if isdir(dirname)
 %   rmdir(dirname,'s');
@@ -16,8 +20,8 @@ function OOCNorm = WriteNorm(dirname,x)
 %assert(status,'Fatal error while creating directory %s',dirname);
 
 % Create a brand new file for serial I/O 
-% delete('NormFile');
-% java.io.RandomAccessFile('NormFile','rw');
+ delete('NormFile');
+ myfile = java.io.RandomAccessFile('NormFile','rw');
 
 % Create a Java object array
 % Java Arr = array of arrays
@@ -46,14 +50,22 @@ j2mArray = cell2mat(cell(javaArr))
 % Calc norm of column vector
 newcol1 = j2mArray(:,1)
 n1 = norm(newcol1)
+myfile.write(n1) 
+myfile.close()
 
-% Convert Mat to Java - Not easy
-objArr = beta.javaseis.array.ArrayUtil.allocateArray(beta.javaseis.array.ElementType.DOUBLE,length(newcol1))
-java.lang.System.arraycopy(newcol1,0,objArr,0,length(newcol1))
+% Use JS/Java calls
+% TEST Case : Algo
+x = [23,34,6,67]
+slice(1) = 4 ;
+slice(2) = 5 ;
+range(1) = 6 ;
+range(2) = 8 ;
+header = SDCpckg.io.JavaSeis.serial.HeaderWrite(x,'double',0)
+SDCpckg.io.JavaSeis.serial.FileAlloc('newtest',header) ;
+SDCpckg.io.JavaSeis.serial.FileWrite('newtest',x) ;
+[myslice header] = SDCpckg.io.JavaSeis.serial.FileReadLeftSlice('newtest',slice) ;
+[mychunk header] = SDCpckg.io.JavaSeis.serial.FileReadLeftChunk('newtest',range,slice) ;
+nbTrc = size(mychunk) ;
+norm = beta.javaseis.examples.io.norm2.fileNorm(nbTrc(2),mychunk) ;
 
 
-a = objArr
-b = newcol1
-
-% Create Java Frame Object
-% frame = java.awt.Frame('testframe')
