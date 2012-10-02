caac455b662653bee99f93aaccce8cba083b93a3
Thomas <yuyanglai@yahoo.com>
2012-10-02 15:58:13 -0700

    added applyDivide to iCon

---
+++ @SeisDataContainer/applyDivide.m
@@ -0,0 +1,5 @@
+function y = applyDivide(x,op)
+%APPLYDIVIDE Summary of this function goes here
+%   Detailed explanation goes here
+
+error('mldivide has not been implemented for %s yet', class(x));
---
+++ @iCon/applyDivide.m
@@ -0,0 +1,5 @@
+function y = applyDivide(x,op)
+%APPLYDIVIDE Summary of this function goes here
+%   Detailed explanation goes here
+
+y = mldivide(op, double(x));
