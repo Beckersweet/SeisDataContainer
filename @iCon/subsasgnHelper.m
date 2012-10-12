9216b792839590b466dc310960a0c4e379f25b89
Thomas <yuyanglai@yahoo.com>
2012-10-12 13:00:03 -0700

    moved iCon/subsasgn to SeisDataContainer level

--- @iCon/subsasgnHelper.m
+++ @iCon/subsasgnHelper.m
@@ -1,3 +1,6 @@
-function x = subsasgnHelper(x,s,b);
+function x = subsasgnHelper(x,s,b)
+%SUBSASGNHELPER Helper function to assign container-specific data
+%
+%   Not for user to use.
 
 x.data = subsasgn(x.data,s,b);
