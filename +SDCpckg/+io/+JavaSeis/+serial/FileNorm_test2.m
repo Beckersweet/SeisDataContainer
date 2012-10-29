cb614a7b331c36d42c97da49a9ebed4d59e3a410
Barbara Collignon <barbara.collignon@gmail.com>
2012-10-29 11:08:01 -0700

    Vect to Mat, Java calls

--- +SDCpckg/+io/+JavaSeis/+serial/FileNorm_test2.m
+++ +SDCpckg/+io/+JavaSeis/+serial/FileNorm_test2.m
@@ -1,4 +1,4 @@
-function [x,y,z] = FileNorm_test2(dirname,K,J,norm)
+function [x,y,zz] = FileNorm_test2(dirname,K,J,norm)
 %FILENORM Calculates the norm of a given data
 %
 %   FileNorm(DIRNAME,FILENAME,DIMENSIONS,NORM,FILE_PRECISION)
@@ -33,10 +33,10 @@ test = rand(dimensions) ;
 bytesize  = SDCpckg.utils.getByteSize(file_precision);
 
 % Set the sizes
-dims      = [1 prod(size(test))] 
-reminder  = prod(size(test)) 
-SDCbufferSize = reminder  
-maxbuffer = SDCbufferSize/bytesize 
+dims      = [1 prod(size(test))] ;
+reminder  = prod(size(test)) ;
+SDCbufferSize = reminder  ;
+maxbuffer = SDCbufferSize/bytesize ;
 
 if(norm == 'fro')
     norm = 2;
@@ -44,176 +44,155 @@ end
 
 % Infinite norm
 if(norm == inf)
-    total =0 ;
-    totaltest=0 ;
-    totaltest2=0;
+   
+    total = 0 ;
+    totaltest2 = 0;
     x = -999 ;
     y = -999 ;
+    z = 0;
+    rstart = 1;
+    buffer = min(reminder,maxbuffer);
+    rend = rstart + buffer - 1;
     
-    z = 0; 
-  %  matarr1D = dims 
-    
-   
-         for k = 1:K
-           for j = 1:J-2
-              
-               r = SDCpckg.io.JavaSeis.serial.FileReadLeftChunk(dirname,[j j+2],[k 1]) ;
-               size_r= size(r);
-               %test_r = r;
-               
-               % compare alapsed times between methods
-               % telapsed1 < telapsed3 < telapsed2
-               %tStart1 = tic 
-               % sum all elements per row
-               
-               
-               for i=1:size_r(1)
+    while (reminder > 0 && rend < SDCbufferSize)
+        buffer = min(reminder,maxbuffer);
+        rend = rstart + buffer - 1;
+        
+        % We expect the Chunk to be already a vector
+        r =  SDCpckg.io.JavaSeis.serial.DataReadLeftChunk(dirname,[1 K],[],[rstart rend],'single') ;
+        size_r= size(r);
+        
+         for i=1:size_r(1)
+             for t=1:size_r(2)
                   
-                   for t=1:size_r(2)
-                   % Convert Matrix to Vector
-                   %a = r(i,t)
                    matarr1D(1+z) = r(i,t) ;
                    z=z+1 ;
                   
-                   end
-                   %total    = sumabs(r(i,:)) ;
-                   % take the higher sum among rows
-                   %x     = max(total,x);
-                   
-               end
-               %tElapsed1 = toc(tStart1) 
-              
+             end
                
-               % No Need to transpose r (row to col)
-               
-               %tStart2 = tic 
-               % totaltest = beta.javaseis.examples.io.sum_inf.fileNormInf(r) ;
-               %tElapsed2 = toc(tStart2) 
-               % y     = max(totaltest,y);
-               % tElapsed2 = toc(tStart2) ; 
-              
-             
-               %if length(size(r))>1
-               %    r = reshape(r,[1 prod(size(r))]);
-               %end
-               
-               %tStart3 = tic
-               %totaltest2 = beta.javaseis.examples.io.sum_inf.fileNormVectInf(r) ;
-               %y     = max(totaltest2,y);
-               %tElapsed3 = toc(tStart3)
-               % replace/compare for loop with JS/JAVA call 
-               % need to calculate vector norm instead of matrix
-               % chunk should be a vector 
-              
-               clear r
-          
-           end
          end
-        
-     size(matarr1D)
+         
+        total     = max(abs(r));
+        x         = max(total,x);        
+        reminder  = reminder - buffer;
+        rstart    = rend + 1;
+        clear r;
+    end
+     
+     
+    
+   
      totaltest2 = beta.javaseis.examples.io.sum_inf.fileNormVectInf(matarr1D) ;
      y     = max(totaltest2,y);
-   
+     zz = y;
     
 % Negative infinite norm    
 elseif(norm == -inf)
+ 
     total =0 ;
-    totaltest=0 ;
-    x = 999 ;
-    y = 999 ;
-    z = 0; 
+    totaltest2 = 0;
+    rstart = 1;
     
-           for k = 1:K
-           for j = 1:J-2
-              
-               r = SDCpckg.io.JavaSeis.serial.FileReadLeftChunk(dirname,[j j+2],[k 1]) ;
-               size_r= size(r);
-               %test_r = r;
-               
-               % compare alapsed times between methods
-               % telapsed1 < telapsed3 < telapsed2
-               %tStart1 = tic 
-               % sum all elements per row
-               
-               
-               for i=1:size_r(1)
+    x = inf;
+    y = inf;
+    z = 0;
+    buffer = min(reminder,maxbuffer);
+    rend = rstart + buffer - 1;
+    
+    while (reminder > 0 && rend < SDCbufferSize)
+        buffer = min(reminder,maxbuffer);
+        rend = rstart + buffer - 1;
+        
+        % We expect the Chunk to be already a vector
+        r =  SDCpckg.io.JavaSeis.serial.DataReadLeftChunk(dirname,[1 K],[],[rstart rend],'single') ;
+        size_r= size(r);
+        
+         for i=1:size_r(1)
+             for t=1:size_r(2)
                   
-                   for t=1:size_r(2)
-                   % Convert Matrix to Vector
-                   %a = r(i,t)
                    matarr1D(1+z) = r(i,t) ;
-                   z=z+1;
+                   z=z+1 ;
                   
-                   end
-                  
-                   
-               end
-              
-               clear r
-          
-           end
+             end
+               
          end
-        
-     size(matarr1D)
-     totaltest2 = beta.javaseis.examples.io.sum_inf.fileNormVectPInf(matarr1D) ;
-     y     = min(totaltest2,y);      
+         
+        total     = min(abs(r));
+        x         = min(total,x);        
+        reminder  = reminder - buffer;
+        rstart    = rend + 1;
+        clear r;
+    end
     
-         %for k = 1:K
-         %  for j = 1:J-2
-            
-         %      r = SDCpckg.io.JavaSeis.serial.FileReadLeftChunk(dirname,[j j+2],[k 1]) ;
-         %     size_r= size(r) ;
-         %     test_r = r ;
-               
-               % sum all elements per row
-         %     for i=1:size_r(1)
-         % 
-         %         r_row = r(i,:) ;
-         %          total    = sumabs(r(i,:)) ;
-                   % take the higher sum among rows
-         %          x     = min(total,x);
-         %      end
-               
-               
-               
-               % No Need to transpose r (row to col)
-               
-               
-         %      totaltest = beta.javaseis.examples.io.sum_inf.fileNormPInf(r) ;
-               
-         %       y     = min(totaltest,y);
-              
-             
-              
-               
-               % replace/compare for loop with JS/JAVA call 
-               % need to calculate vector norm instead of matrix
-               % chunk should be a vector 
-              
-         %      clear r
-          
-         %  end
-         %end
-        
+    
+    totaltest2 = beta.javaseis.examples.io.sum_inf.fileNormVectPInf(matarr1D) ;
+    y = min(totaltest2,y) ;
+    zz=y;
+  %  x = total^(1/norm);
        
-    
-    
 % P-norm
 elseif (isscalar(norm))
-    total = 0;
-    totaltest = 0;
-    totaltest2 = 0;
-    z = 0;    
+ 
+    
+    total = 0 ;
+    totaltest2 = 0 ;
+    z=0;
+    anothertest = 0 ;
+    rstart = 1;
+    buffer = min(reminder,maxbuffer);
+    rend = rstart + buffer - 1;
+    while (reminder > 0 && rend < SDCbufferSize)
+        buffer = min(reminder,maxbuffer);
+        rend = rstart + buffer - 1;
         
-         for k = 1:K
-           for j = 1:J-2
+        % We expect the Chunk to be already a vector
+        r =  SDCpckg.io.JavaSeis.serial.DataReadLeftChunk(dirname,[1 K],[],[rstart rend],'single') ;
+        size_r= size(r);
+        
+        % FileNormScalar works for 2D only
+        anothertest = anothertest + beta.javaseis.examples.io.sum_inf.fileNormScalar(r,norm) ;
+        
+         for i=1:size_r(1)
+             for t=1:size_r(2)
+                  
+                   matarr1D(1+z) = r(i,t) ;
+                   z=z+1 ;
+                  
+              end
+           
+         end
+        
+        
+      %  total    = total + sum(abs(r).^norm);
+        reminder = reminder - buffer;
+        rstart   = rend + 1;
+        clear r;
+    end
+    
+    zz = anothertest^(1/norm) 
+    
+    totaltest2 = beta.javaseis.examples.io.sum_inf.fileNormVectScalar(matarr1D,norm) ;
+    y = totaltest2^(1/norm);
+    
+    x = total^(1/norm);
+    
+ %   total = 0;
+ %   totaltest = 0;
+ %   totaltest2 = 0;
+ %   z = 0;    
+ %   test = 0;    
+    
+ %        for k = 1:K
+ %          for j = 1:J-2
             
-               r = SDCpckg.io.JavaSeis.serial.FileReadLeftChunk(dirname,[j j+2],[k 1]) ;
-               size_r= size(r);
+ %              r = SDCpckg.io.JavaSeis.serial.FileReadLeftChunk(dirname,[j j+2],[k 1]) ;
+ %              size_r= size(r)
+ %              test = test + 1 
+               
               
-               for i=1:size_r(2)
-                total    = total + sum(abs(r(:,i)).^norm) ;
-               end
+ %              for i=1:size_r(2)
+ %               total    = total + sum(abs(r(:,i)).^norm) ;
+ %              end
                
               % totaltest = totaltest+ beta.javaseis.examples.io.sum_inf.fileNormScalar(r,norm) ;
                 
@@ -222,37 +201,37 @@ elseif (isscalar(norm))
               % end
                
                
-                for i=1:size_r(1)
+  %              for i=1:size_r(1)
                   
-                   for t=1:size_r(2)
+  %                 for t=1:size_r(2)
                    % Convert Matrix to Vector
                    % Be careful: Vector non-contigus in memory
                    % Meaning the size of the vector is larger
                    %a = r(i,t)
-                   matarr1D(1+z) = r(i,t) ;
-                   z=z+1
+  %                 matarr1D(1+z) = r(i,t) ;
+  %                 z=z+1 ;
                   
-                   end
+  %                 end
                    %total    = sumabs(r(i,:)) ;
                    % take the higher sum among rows
                    %x     = max(total,x);
                    
-               end
+  %             end
                
                
               %totaltest2 = totaltest2 + beta.javaseis.examples.io.sum_inf.fileNormVectScalar(matarr1D,norm) ;
               
-              clear r
+  %             clear r;
           
-           end
-         end
+  %         end
+  %       end
          
-     SIZE =    size(matarr1D)
+  %   SIZE =    size(matarr1D);
      
-    totaltest2 = beta.javaseis.examples.io.sum_inf.fileNormVectScalar(matarr1D,norm) ;
-    y = totaltest2^(1/norm);  
+  %  totaltest2 = beta.javaseis.examples.io.sum_inf.fileNormVectScalar(matarr1D,norm) ;
+  %  y = totaltest2^(1/norm);  
   
-    x = total^(1/norm);
+  %  x = total^(1/norm);
 else
     error('Unsupported norm');
 end
