c08eddbca600b473a8c0859ca71819760281e622
Barbara Collignon <barbara.collignon@gmail.com>
2012-11-05 14:27:48 -0800

    norm fixes
    to be completed

--- +SDCpckg/+io/+JavaSeis/+serial/+JavaFunc/normTot.java
+++ +SDCpckg/+io/+JavaSeis/+serial/+JavaFunc/normTot.java
@@ -1,244 +1,295 @@
 package beta.javaseis.examples.io;
 
-import java.io.File;
+import java.util.Iterator;
 
-import java.util.Arrays;
-
-import beta.javaseis.array.ElementType;
-import beta.javaseis.array.IMultiArray;
-import beta.javaseis.array.MultiArray;
-import beta.javaseis.grid.GridDefinition;
 import beta.javaseis.io.Seisio;
-import beta.javaseis.parset.ParameterSetIO;
 import beta.javaseis.util.SeisException;
-
-import edu.mines.jtk.util.ArrayMath;
-import edu.mines.jtk.util.ParameterSet;
-
-/*
- *  Norm Test Case :
- *  Create a  JS datasets containing Random numbers
- *  Loop Over the Slices of the Dataset
- *  Calculate the Norm Component (sum) for the current Slice
- *  Gather All Norm Components to calculate the final Norm. 
- *  
- */
-
 
 public class normTot {
 
-  /**
-   * @param args
-   * @throws SeisException 
-   */
-  public static void main(String[] args) throws SeisException {    
-
-	 // Create Random File - DATASET of size  [250, 30, 100, 10]
-	 createRandomFile(args) ;  
-	  
-     // Convert arguments to a ParameterSet object for easy access
-     ParameterSet parset = ParameterSetIO.argsToParameters( args );
-    
-     // Get path name for new dataset - default to "jsCreateTest" in User.home
-     String path = parset.getString("path", System.getProperty("user.home") + File.separator + 
-        "jsCreateTest" );
-
-     System.out.println("Calculate Norm frame value of JavaSeis dataset\nPath: " + path );
-   
-    
-     Seisio sio = new Seisio(path);
-     sio.open("r");
- 
-     // Define number of Hypercubes, Volumes, Frames & Traces
-     long[] Axis = sio.getGridDefinition().getAxisLengths() ;
-  
-     System.out.println("Axis1 = " + Axis[0]) ; // 250
-     System.out.println("Axis2 = " + Axis[1]) ; // 30
-     System.out.println("Axis3 = " + Axis[2]) ; // 100
-     System.out.println("Axis4 = " + Axis[3]) ; // 10
-    
-     double normComp = 0.0 ; 
-     int norm = 2 ;
- 
-     int i;
-     int j;
-  
-     //for(myslice_m to myslice_m+n)
-     for(j=0;j<Axis[3];j++) {
-	     for(i=0;i<Axis[2];i++)  {
-	
-	    	int[] pos = new int[] {0,0,i,j} ;
-     	//	int[] dims = new int[] {0,1} ;
-		
-            normComp += sliceNormComponent(sio,pos,norm);
-       
-            System.out.println("NormComp frame Value of dataset = " + normComp) ;
-     
-     	}
-       }
-  //end
-      double normTot = 0 ;
-  
-      if(norm == 1 || norm == 0 || norm == 2) {
-         //norm scalar   
-	     double exp = 1/(double)norm ;
-	     normTot = Math.pow(normComp,exp) ; 
-	     //System.out.println("Norm frame Value of dataset lalala = " + normComp) ;
-   
-      } else if (norm == 999) {
-	    // norm inf
-	     normTot = Math.max(normComp,-999.00);
-	     //System.out.println("Norm frame Value of dataset = " + normTot) ;
-   
-      } else if (norm == -999) {
-	    // norm -inf 
-	     normTot = Math.min(normComp,999.00) ;
-	     //System.out.println("Norm frame Value of dataset = " + normTot) ;
-	
-  }
-  
-   
-   System.out.println("Norm frame Value of dataset = " + normTot) ;
-   
-   sio.close() ;
-   
-  }
-  
-  public static void createRandomFile(String[] args) throws SeisException {
-	
-	  // Convert arguments to a ParameterSet object for easy access
-	    ParameterSet parset = ParameterSetIO.argsToParameters(args);
-	    // Get path name for new dataset - default to "jsCreateTest" in User.home
-	    String path = parset.getString("path", System.getProperty("user.home")
-	        + File.separator + "jsCreateTest");
-
-	    System.out
-	        .println("Create JavaSeis CDP dataset with random numbers\nPath: "
-	            + path);
-
-	  
-	  // Get the size of the dataset to be created
-	    int[] size = parset.getInts("size", new int[] { 250, 30, 100, 10 });
-	    if (size.length != 4) {
-	      throw new RuntimeException(
-	          "Wrong number of elements for size - should be 4");
-	    }
-
-	    System.out.println("Size: " + Arrays.toString(size));
-	    // Get the spacing of the dataset to be created
-
-	    double[] deltas = parset.getDoubles("deltas",
-	        new double[] { 4, 100, 25, 50 });
-	    if (deltas.length != 4) {
-	      throw new RuntimeException(
-	          "Wrong number of elements for deltas - should be 4");
-	    }
-
-	    long[] ldeltas = parset.getLongs("ldeltas", new long[] { 4, 4, 1, 2 });
-	    if (ldeltas.length != 4) {
-	      throw new RuntimeException(
-	          "Wrong number of elements for ldeltas - should be 4");
-	    }
-
-	    // Get the origins of the dataset to be created
-	    double[] origins = parset
-	        .getDoubles("origins", new double[] { 0, 0, 0, 0 });
-	    if (origins.length != 4) {
-	      throw new RuntimeException(
-	          "Wrong number of elements for origins - should be 4");
-	    }
-
-	    long[] lorigins = parset.getLongs("lorigins", new long[] { 0, 1, 1, 1 });
-	    if (lorigins.length != 4) {
-	      throw new RuntimeException(
-	          "Wrong number of elements for lorigins - should be 4");
-	    }
-
-	    // Make a GridDefinition
-	    GridDefinition grid = GridDefinition.standardGrid(GridDefinition.CDP, size,
-	        lorigins, ldeltas, origins, deltas);
-
-	    // Be sure that it doesn't already exist (which is an error).
-	    Seisio.delete(path);
-
-	    // Attempt to create
-	    Seisio sio = new Seisio(path, grid);
-	    sio.create();
-	    System.out.println("Created " + path);
-	    // Use a MultiArray to hold data for writing
-
-	    IMultiArray frm = MultiArray.factory(2, ElementType.FLOAT, 1, size);
-
-	    float[] trc = new float[size[0]];
-	    // Loop and write random numbers
-	    int[] position = new int[4];
-	    for (int volume = 0; volume < size[3]; volume++) {
-	      System.out.println("Writing volume " + volume + " ... ");
-	      position[3] = volume;
-	      for (int frame = 0; frame < size[2]; frame++) {
-	        position[2] = frame;
-	        for (int trace = 0; trace < size[1]; trace++) {
-	          ArrayMath.rand(trc);
-	          position[1] = trace;
-	          frm.putTrace(trc, position);
-	        }
-	        position[0] = position[1] = 0;
-	        sio.writeMultiArray(frm, position);
-	      }
-	    }
-
-	    // Close and that's all, folks !
-	    sio.close();
-	    System.out.println("Write complete - yeah");
-	  
-  }
-  
-  
   public static double sliceNormComponent(Seisio sio, int[] position,int norm) throws SeisException {
-	 
-     
-	  //  Seisio sio = new Seisio(path);
-	  //  sio.open("r");
-	    sio.readFrame(position) ;
-	  
+ 
+	    int ntrc = sio.readFrame(position) ;
+	//  System.out.println(Arrays.toString(position));
 	    
 	    double f = 0.0 ;
-	   
+	//  int nbElt = 0 ;
+	    double min = 999.00;
+		double min2 = 999.00;
 	    		
+		double max = -999.00;
+		double max2 = -999.00;
+		
 	    float[][] trc = sio.getTraceDataArray();
 	    
-    
+	    for (int j=0; j<ntrc; j++) {
+	    	
+	    	double[] vals = new double[trc[j].length] ;
+	    	
+	        for (int i=0; i<trc[j].length; i++) {
     		    
+	//        	nbElt+=1;
+	        	
     			if(norm == -999) {
     		    	
-    				f  = fileNormInf(trc) ;
-    				
+    				vals[i]=Math.abs((double)(trc[j][i]));
+    				min2 = Math.min(min2, vals[i]);
+    				  
+    				min = Math.min(min,min2); 
+    				       
+    				f = (double)min ;
+    			
     		    } else if (norm == 999) {
-    		    	
-    		    	f  = fileNormPInf(trc) ;
-    		    	
+    		    
+    				vals[i]=Math.abs((double)(trc[j][i]));
+    				max2 = Math.max(max2, vals[i]);
+    			
+    				max = Math.min(max,max2); 
+    				    
+    		        f = (double)max ;
+    		      
     		    } else if (norm == 0) {
     		    
-    		        f = fileNormScalar(trc,0) ;
+    		    	f+=(double)(1);
     		    	
     		    } else if (norm == 1) {
     		    	
-    		    	 f = fileNormScalar(trc,1) ;
+    		    	f+=(double)(trc[j][i]);
     		    	
     		    } else if (norm == 2) {
     		    	
-    		     f = fileNormScalar(trc,2) ;
+    		    	f+=(double)(trc[j][i]*trc[j][i]);
     		    	
     		    }
       		
-      	
-	//  sio.close() ;
-	    
+	        }
+	    }
+	
+	//    System.out.println("nbElt " + nbElt);  
 	
 	  return f ;
 	
   
 }
+
+  public static double FileNorm(String path,int norm) throws SeisException {
+		 
+	     
+	    Seisio sio = new Seisio(path);
+	    sio.open("r");
+	    
+	    double f = 0.0 ;
+        double finallala = 0.0;
+        
+        double min = 999.00;
+		double min2 = 999.00;
+		
+		double max = -999.00;
+		double max2 = -999.00;
+	
+	     // Define number of Hypercubes, Volumes, Frames & Traces
+	     long[] Axis = sio.getGridDefinition().getAxisLengths() ;
+	    	  
+	     System.out.println("nbSam = " + Axis[0]) ; // 250
+	     System.out.println("nbTrc = " + Axis[1]) ; // 30
+	     System.out.println("nbFra = " + Axis[2]) ; // 100
+	     System.out.println("nbVol = " + Axis[3]) ; // 10
+	    
+	     
+	     int[] pos = {0,0,0,0};
+	     
+	     int ntrc = sio.readFrame(pos);
+	     
+	     float[][] trc = sio.getTraceDataArray();
+	    
+	 
+	   System.out.println("number of traces " + ntrc);
+	    
+	   if(norm == -999) {
+		   
+		   for (int j=0; j<ntrc; j++) {
+			   
+			   double[] vals = new double[trc[j].length] ;
+			   
+		        for (int i=0; i<trc[j].length; i++) {
+		        	
+		        	  vals[i]=Math.abs((double)(trc[j][i]));
+		              min2 = Math.min(min2, vals[i]);
+		        	
+		        }
+		        
+		       min = Math.min(min,min2); 
+		     
+		      }
+		   
+		//   f = (double)min;
+		   
+		   
+	   } else if (norm == 999) {
+		   
+               for (int j=0; j<ntrc; j++) {
+			   
+			       double[] vals = new double[trc[j].length] ;
+			   
+		           for (int i=0; i<trc[j].length; i++) {
+		        	
+		        	  vals[i]=Math.abs((double)(trc[j][i]));
+		        	  max2 = Math.max(max2, vals[i]);
+		        	
+		        }
+		        
+		           max = Math.min(max,max2); 
+		          
+		      } 
+		   
+          //  f = (double)max;
+		   
+	   } else if (norm == 0) {
+		   
+		   for (int j=0; j<ntrc; j++) {
+		        for (int i=0; i<trc[j].length; i++) {
+		        	
+		        	f+=(double)(1);
+		       
+		        	
+		        }
+		      }
+		   
+	   } else if (norm == 1) { 
+			   
+		   for (int j=0; j<ntrc; j++) {
+		        for (int i=0; i<trc[j].length; i++) {
+		        	
+		        	f+=(double)(trc[j][i]);
+		       
+		        	
+		        }
+		      }	   
+			   
+      } else if (norm == 2) {
+		   
+		   
+	   
+	    for (int j=0; j<ntrc; j++) {
+	        for (int i=0; i<trc[j].length; i++) {
+	        	
+	        	f+=(double)(trc[j][i]*trc[j][i]);
+	       
+	        	
+	        }
+	      }
+	   
+       }
+	    
+	    Iterator<int[]> frames = sio.frameIterator();
+	   
+	    // Loop over frames and calculate Norm value
+	    while (frames.hasNext()) {
+	    //  nbF+=1	;
+	    	
+	      frames.next();
+	     
+	      ntrc = sio.getTracesInFrame();
+	      
+	      if(norm == -999) {
+			  
+			   for (int j=0; j<ntrc; j++) {
+				   
+				   double[] vals = new double[trc[j].length] ;
+				   
+			        for (int i=0; i<trc[j].length; i++) {
+			        	
+			        	  vals[i]=Math.abs((double)(trc[j][i]));
+			              min2 = Math.min(min2, vals[i]);
+			        	
+			        }
+			        
+			       min = Math.min(min,min2); 
+			       
+			      }
+			   
+			   finallala = min ;
+			   
+			   
+	      } else if(norm == 999) {
+	    	 
+	               for (int j=0; j<ntrc; j++) {
+				   
+				       double[] vals = new double[trc[j].length] ;
+				   
+			           for (int i=0; i<trc[j].length; i++) {
+			        	
+			        	  vals[i]=Math.abs((double)(trc[j][i]));
+			        	  max2 = Math.max(max2, vals[i]);
+			        	
+			        }
+			        
+			           max = Math.min(max,max2); 
+			      } 
+	               
+	               
+	             finallala = max ;  
+	               
+	      } else if (norm == 0) {
+    		    
+
+		      double exp = 1/(double)norm ;
+	    	  
+	    	  for (int j=0; j<ntrc; j++) {
+				   for (int i=0; i<trc[j].length; i++) {
+	    	  
+    		    	f+=(double)(1);
+    		    	
+				   }
+	    	  }
+    		    
+    			    finallala = Math.pow(f,exp) ; 
+    		    	
+    	  } else if (norm == 1) {
+    		  
+
+		     double exp = 1/(double)norm ;
+    		  
+    		  for (int j=0; j<ntrc; j++) {
+				   for (int i=0; i<trc[j].length; i++) {
+	    	  
+    		    	
+    		    	f+=(double)(trc[j][i]);
+    		    	
+				   }
+    		  }
+    		    
+    			    finallala = Math.pow(f,exp) ; 
+    			    
+    		    	
+    	  } else if (norm == 2) {
+    	
+	    	  for (int j=0; j<ntrc; j++) {
+				   for (int i=0; i<trc[j].length; i++) {
+    		  
+    		    	f+=(double)(trc[j][i]*trc[j][i]);
+				   }
+	    	  }
+    		    	finallala = Math.sqrt(f);
+    		   
+    		    	
+    	   }
+    			
+	     }
+	   
+      		
+	  
+      	
+	 // System.out.println("number of FRAMES " + nbF);  
+	    
+      sio.close() ;
+	    
+     // finallala = Math.sqrt(f);
+      
+      
+      return finallala ;
+	
+ }
 
 
 
@@ -294,7 +345,7 @@ public static double fileNormScalar(float [][] trc, double norm) throws SeisExce
  int   n = trc[0].length;
 
    double f = 0.0;
-   double s = 0;
+   double s = 0.0;
     
     for (int i = 0; i < m; i++) {
      //  double s = 0;
