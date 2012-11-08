26eaf51418b37351e6c822298b33de031b8e6efe
Thomas <yuyanglai@yahoo.com>
2012-11-07 17:12:02 -0800

    added new spgl1 tests for iCon and piCon

--- @iCon/spgl1.m
+++ @iCon/spgl1.m
@@ -1,11 +1,15 @@
-function [x,r,g,info] = spgl1( A, b, tau, sigma, x, options )
+function varargout = spgl1( A, b, tau, sigma, x, options )
 %SPGL1 spgl1 unwrapper and rewrapper for iCon
 %   Detailed explanation goes here
 
 if isa(A,'iCon')
-    [x,r,g,info] = spgl1( double(A), b, tau, sigma, x, options )
+    [varargout{1:nargout}] = spgl1( double(A), b, tau, sigma, x, options );
 elseif isa(b, 'iCon')
-    [x,r,g,info] = spgl1( A, double(b), tau, sigma, x, options )
+    [varargout{1:nargout}] = spgl1( A, double(b), tau, sigma, x, options );
 elseif isa(A, 'iCon') && isa(b, 'iCon')
-    [x,r,g,info] = spgl1( double(A), double(b), tau, sigma, x, options )
+    [varargout{1:nargout}] = spgl1( double(A), double(b), tau, sigma, x, options );
+elseif isa(x, 'iCon')
+    [varargout{1:nargout}] = spgl1( A, b, tau, sigma, double(x), options );
+else
+    error('Something was seriously fishy here');
 end
---
+++ tests/spgl1tests/test_piCon_spgl1_real.m
@@ -0,0 +1,71 @@
+function test_suite = test_piCon_spgl1_real
+initTestSuite;
+
+    % BEGIN INITIALIZATION
+
+    function opt = setup
+        % Make sure environment is pristine
+        clear all
+        clear functions
+        
+        % Fix random generator for repeatable experiments
+        defaultStream = RandStream.getDefaultStream;
+        savedState = defaultStream.State;
+        RandStream.setDefaultStream(RandStream('mt19937ar','seed',8888));
+        
+        % Make test cases
+        m = 300;
+        n = 1000;
+        k = 20;
+        
+        A = randn(m,n);
+        x = zeros(n,1);
+        x(floor((n-1)*rand(k,1))+1) = randn(k,1); % inject k-sparse signal in random places
+        b = A*x;
+        
+        % Set sparse recovery element-wise relative tolerance
+        recov_tol = 1e-5;
+        
+        % Inject options into test cases
+        opt.A = A;
+        opt.b = b;
+        opt.x = x;
+        opt.recov_tol = recov_tol;
+        opt.savedState = savedState;
+
+    function teardown(opt)
+        % Restore random stream and restore variables
+        defaultStream.State = opt.savedState;
+        clear opt
+        clear all
+        clear functions
+    
+
+
+    % BEGIN TESTS
+    
+    function testParallelVectorWorks(opt)
+    
+        init_x = piCon.zeros(length(opt.x),1);
+        
+        
+        spg_opts  = spgSetParms('verbosity' ,     0, ...
+                                'subspaceMin',    0);
+        [x, r, g, info] = spgl1(opt.A, opt.b, [], 1e-5, init_x, spg_opts);
+        
+        assertElementsAlmostEqual(undist(x), opt.x, 'absolute', opt.recov_tol);
+        assertTrue(info.iter < 80);
+    
+    function testParallelMatrixVectorWorks(opt)
+        
+        init_x = distributed.zeros(length(opt.x),1);
+        A = piCon(opt.A);
+        b = piCon(opt.b);
+        
+        spg_opts  = spgSetParms('verbosity' ,     0, ...
+                                'subspaceMin',    0);
+        [x, r, g, info] = spgl1(A, opt.b, [], 1e-5, init_x, spg_opts);
+    
+        assertElementsAlmostEqual(undist(x), opt.x, 'absolute', opt.recov_tol);
+        assertTrue(info.iter < 80);
+
