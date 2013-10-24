f369fed5fc3c6245505182f69e404c9cd45568bb
Daniel Hsieh <dhsieh@rtm.eos.ubc.ca>
2013-10-24 14:33:54 -0700

    improved delta.m and unit.m in @SeisDataContainer

--- @SeisDataContainer/delta.m
+++ @SeisDataContainer/delta.m
@@ -1,4 +1,22 @@
-function y = delta(x)
-    y = x.header.delta;
+function y = delta(x,varargin)
+
+y = x.header.delta;
+
+if ~isempty(varargin)
+    
+    len = length(varargin);
+    
+    % converting cell array of integers into double array
+    ind = zeros(1,len);
+    for i = 1 : len
+        ind(i) = varargin{i};
+    end
+    
+    y = y(ind);
+    
 end
+
+
+end
+
 
