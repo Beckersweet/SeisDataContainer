66ca3a821b111c840d13ace94c774a448b8c4a77
Daniel Hsieh <dhsieh@rtm.eos.ubc.ca>
2013-10-25 14:36:33 -0700

    include more test on delta and unit; all tests passed

--- @SeisDataContainer/delta.m
+++ @SeisDataContainer/delta.m
@@ -1,8 +1,17 @@
 function y = delta(x,varargin)
     y = x.header.delta;
-
- 
-
+    
+    if ~isempty(varargin)
+        len = length(varargin);
+        % converting cell array of integers into double array
+        ind = zeros(1,len);
+        for i = 1 : len
+            ind(i) = varargin{i};
+        end
+            
+        y = y(ind);
+    end
+    
 end
 
 
--- @SeisDataContainer/unit.m
+++ @SeisDataContainer/unit.m
@@ -1,7 +1,16 @@
 function y = unit(x,varargin)
     y = x.header.unit;
-
-
-
+    
+    if ~isempty(varargin)
+        len = length(varargin);
+        % converting cell array of integers into double array
+        ind = zeros(1,len);
+        for i = 1 : len
+            ind(i) = varargin{i};
+        end
+            
+        y = y(ind);
+    end
+    
 end
 
