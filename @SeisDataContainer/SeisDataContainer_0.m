0d0ca4fcc2f5007f2667fccbb74f334285701a2d
Daniel Hsieh <dhsieh@rtm.eos.ubc.ca>
2013-11-26 15:12:32 -0800

    include the original code before brached from master

---
+++ @SeisDataContainer/SeisDataContainer_0.m
@@ -0,0 +1,125 @@
+classdef SeisDataContainer
+%SEISDATACONTAINER  The Data Container Mother Class
+%
+%   x = SeisDataContainer(TYPE,EXSIZE,IMSIZE) is the abstract parent class for
+%   all data container classes.
+%
+%   TYPE   is a string that specifies the type of the data container that 
+%          is being created
+%   EXSIZE is the explicit dimensions of the contained data
+%   IMSIZE is the implicit dimensions of the contained data
+%
+%   Data Container Flags:
+%   strict If true, all elementary operations must be performed on data
+%          container objects that have the exact same implicit dimensions
+%          as well as permutation.
+%
+%   Since this is an abstract class that cannot be instantiated, this
+%   documentation is for reference purposes only.
+%
+%   Data Container subclasses: 
+%   iCon, piCon
+%
+%   Overloaded Matlab methods:
+%   abs, eq, isreal, length, ndims, plus, size, ge, isempty, 
+%   isscalar, lt, ne, power, subsasgn, double, gt, isize, ldivide, minus,
+%   norm, rdivide, subsref, end, isnumeric, le, mtimes, normest%   
+%
+%   Extra methods not found in Matlab:
+%   invvec, invpermute, setImSize, vec
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Properties
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    properties (SetAccess = protected)
+        header = struct(); % header struct for dataContainer
+        exsize = []; % Explicit dimensions of data
+        perm   = []; % Permutation of data (since original construction)
+        type   = ''; % Type of data container
+        strict = false; % Strict flag for elementary operations
+    end
+    
+    properties ( Access = protected )
+        data   = []; % Actual data for the container
+    end
+        
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Public Methods
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    methods
+        
+        % DataCon Constructor
+        function x = SeisDataContainer(headerIn,varargin)
+                                               
+            % Set attributes
+            x.header = headerIn;
+            x.exsize = [1:length(x.header.size);...
+                        1:length(x.header.size)];
+
+            % parse extra arguments
+            ldims = length(x.header.size);
+            p = inputParser;
+            p.addParamValue('varName',x.header.varName,@ischar);
+            p.addParamValue('varUnits',x.header.varUnits,@ischar);
+            p.addParamValue('origin',x.header.origin,@(x)isrow(x)&&length(x)==ldims);
+            p.addParamValue('delta',x.header.delta,@(x)isrow(x)&&length(x)==ldims);
+            p.addParamValue('unit',x.header.unit,@(x)iscell(x)&&length(x)==ldims);
+            p.addParamValue('label',x.header.label,@(x)iscell(x)&&length(x)==ldims);
+            p.parse(varargin{:});
+            x.header.varName  = p.Results.varName;
+            x.header.varUnits = p.Results.varUnits;
+            x.header.origin   = p.Results.origin;
+            x.header.delta    = p.Results.delta;
+            x.header.unit     = p.Results.unit;
+            x.header.label    = p.Results.label;
+            
+        end % Constructor
+                
+    end % Public methods
+        
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Abstract Public Methods
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    methods ( Abstract )
+        
+        % double
+        y = double(x)
+        
+        % plus
+        y = plus(A,B,swp)
+        
+        % minus
+        y = minus(A,B,swp)
+        
+        % mtimes
+        y = mtimes(A,B,swp)
+        
+        % ldivide
+        y = ldivide(A,B)
+        
+        % rdivide
+        y = rdivide(A,B)
+        
+        % power
+        y = power(A,B)
+        
+        % metacopy
+        y = metacopy(x,y)
+                
+    end % Public Abstract methods
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Abstract Static Methods
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    methods ( Abstract, Static )
+        
+        % randn
+        x = randn(varargin);
+        
+        % zeros
+        x = zeros(varargin);
+                
+    end % Abstract static methods
+    
+    
+end
---
+++ @iCon/subsrefHelper_0.m
@@ -0,0 +1,111 @@
+function y = subsrefHelper(x,s)
+%SUBSREFHELPER  Helper function for processing iCon subref
+%
+%   Interface that handles the propagation of header data in datacon.
+%   Nothing the user needs to know here.
+%
+
+lastdim = length(s.subs);
+% Checking indices
+% We need to extract a vector of indices for use in header subsref.
+if length(s.subs) == 1  % Vectorizing case
+    if all(s.subs{:} == ':')
+        y = vec(x);
+        return;
+    end
+    
+else % multiple dims case    
+    % indexing must be same # of dimensions as x
+    if ~(length(s.subs)==size(x.exsize,2))
+        warning('iCon:IndexOutOfBounds',['You were using an indices with ',...
+            'different number of dimensions as the data container??!! ',...
+            'What were you thinking??? Metadata destroyed.']);
+        y = construct(x,subsref(x.data,s));
+        return;
+    end
+    
+    vectorized = false;
+    colonized  = false;
+    
+    for i = length(s.subs):-1:1 % Start from slowest dimension
+        % For index in every explicit dimension, do
+        % case scalar c (single element)
+        if isscalar(s.subs{i}) && isnumeric(s.subs{i})
+            if vectorized || colonized
+                warning('iCon:ScalarRightMostDims',[ 'You have angered ',...
+                    'the God of SeisDataContainer by using non-scalars ',...
+                    'to index over the rightmost dimensions. Your metadata ',...
+                    'has been sacrificed to appease His Containedness']);
+                y = construct(x,x,subsref(x.data,s));
+                return;
+            end
+            
+            lastdim = lastdim - 1;
+            
+        % case vector a:b
+        elseif isvector(s.subs{i}) && isnumeric(s.subs{i})
+            if colonized
+                warning('iCon:ColonRightOfVec',['Guess what cannot be ',...
+                    'used right of vector indexing? colon indexing! Metadata ',...
+                    'is destroyed']);
+                y = construct(x,subsref(x.data,s));
+                return;
+            end
+            
+            a     = s.subs{i}(1);
+            b     = s.subs{i}(end);
+            xdims = x.exsize(:,i);
+            xdims = xdims';
+            
+            % Check for index skipping
+            if ~(length(s.subs{i}) == b - a + 1)
+                warning('iCon:IndexSkipping',['You get into trouble when ',...
+                    'you skip the line, so does your index. ',...
+                    'Metadata is destroyed']);
+                y = construct(x,subsref(x.data,s));
+                return;
+            end
+            
+            % a & b must mod cleanly with the products of faster dimensions
+            for j=1:length(xdims)-1
+                c = x.header.size(xdims(j));
+                if ~(mod(a,c)==1 && mod(b,c)==0)
+                    warning('iCon:CrossIndexingError',...
+                    ['Weep in utter despair, for your metadata is ',...
+                    'destroyed due to cross dimensional indexing']);
+                    y = construct(x,subsref(x.data,s));
+                    return;
+                end
+            end
+            
+            vectorized = true;
+            
+        % case colon:
+        elseif s.subs{i} == ':'            
+            colonized = true;
+            
+        % What??? I don't even...
+        else
+            warning('iCon:UnsupportedIndexing',...
+            ['Take a deep breath, for you are venturing into ',...
+            'unsupported indexing territory. All metadata is destroyed ',...
+            'and made anew']);
+            y = construct(x,subsref(x.data,s));
+            return;
+        end        
+    end    
+end
+
+% Data processing
+data = subsref(x.data,s);
+%lastdim = lastdim - 1;
+
+% Repackage and export
+y                      = construct(x,data);
+y.header.delta         = x.header.delta(1:lastdim);
+y.header.origin        = x.header.origin(1:lastdim);
+y.header.precision     = x.header.precision;
+y.header.complex       = x.header.complex;
+y.header.unit          = x.header.unit(1:lastdim);
+y.header.label         = x.header.label(1:lastdim);
+y.header.distributedIO = x.header.distributedIO;
